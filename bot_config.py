38f55d96da7cbe5e4f9b79c45db4ef36def3be80
Elias_Oliveira <eliasoliveiradapaz@hotmail.com>
2025-03-27 00:29:49 -0300

    add: func repeat rerification

--- bot_config.py
+++ bot_config.py
@@ -1,6 +1,8 @@
 import telebot
 import scrapping_vagas2
+import time
 import threading
+
 
 bot = telebot.TeleBot('7665199891:AAFfH_oEGDnCom1f1Grs4QmcUeImF9ORS6g')
 
@@ -14,22 +16,37 @@ def start(msg:telebot.types.Message):
     resposta = scrapping_vagas2.verify_vacancy()
     bot.reply_to(msg,resposta)
 
+executando = False
+
 def verificacao(msg):
+    global executando
+    while executando:
+        try:
+            resultado = scrapping_vagas2.verify_vacancy()  # Chama a função do outro script
+            if resultado == "Vagas Disponíveis":
+                bot.send_message(msg.chat.id, f"🔍 Resultado da verificação: {resultado}")
+        except Exception as e:
+            bot.send_message(msg.chat.id, f"❌ Ocorreu um erro: {str(e)}")
+        
+        time.sleep(50)  # Aguarda 50 segundos antes da próxima verificação
 
-    try:
-        resultado = scrapping_vagas2.verify_vacancy()  # Chama a função do outro script
-        bot.send_message(msg.chat.id, f"🔍 Resultado da verificação: {resultado}")
-    except Exception as e:
-        bot.send_message(msg.chat.id, f"❌ Ocorreu um erro: {str(e)}")
+@bot.message_handler(commands=['vagas_temp'])
+def start(msg: telebot.types.Message):
+    global executando
+    if not executando:
+        executando = True
+        bot.reply_to(msg, "⏳ A verificação foi iniciada! O bot verificará as vagas a cada 50 segundos.")
+        thread = threading.Thread(target=verificacao, args=(msg,))
+        thread.daemon = True  # Permite encerrar a thread quando o programa terminar
+        thread.start()
 
+@bot.message_handler(commands=['parar_vagas'])
+def stop(msg: telebot.types.Message):
+    global executando
+    executando = False
+    bot.reply_to(msg, "⏹ A verificação de vagas foi interrompida.")
 
-@bot.message_handler(['vagas_temp'])
-def start(msg:telebot.types.Message):
-    
-    bot.reply_to(msg, "⏳ Aguarde 50 segundos para a verificação...")
-
-    temporizador = threading.Timer(20, verificacao,[msg])
-    temporizador.start()
+bot.infinity_polling()
 
 
 bot.infinity_polling()
